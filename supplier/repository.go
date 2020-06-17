2713362aa72adaa53c49b3c273d10a5f30b27042
Arham Abiyan <arham.abiyan@gmail.com>
2020-06-17 17:35:25 +0700

    rewrite repositories

--- supplier/repository.go
+++ supplier/repository.go
@@ -1,4 +1,11 @@
 package supplier
 
+import (
+	"context"
+
+	"github.com/arham09/conn-db/supplier/models"
+)
+
 type Repository interface {
+	FetchAll(ctx context.Context) (res []*models.Supplier, err error)
 }
--- supplier/repository/supplier_pg.go
+++ supplier/repository/supplier_pg.go
@@ -1,31 +1,63 @@
 package repository
 
 import (
+	"context"
 	"database/sql"
 
+	"github.com/arham09/conn-db/supplier"
 	"github.com/arham09/conn-db/supplier/models"
+	"github.com/sirupsen/logrus"
 )
 
-func AllSuppliers(db *sql.DB) ([]*models.Supplier, error) {
-	rows, err := db.Query("SELECT id, code, name, address, status FROM suppliers")
+type pgSupplierRepository struct {
+	Conn *sql.DB
+}
+
+// NewPgArticleRepository will create an object that represent the supplier.Repository interface
+func NewPgSupplierRepository(Conn *sql.DB) supplier.Repository {
+	return &pgSupplierRepository{Conn}
+}
+
+func (p *pgSupplierRepository) fetch(ctx context.Context, query string, args ...interface{}) ([]*models.Supplier, error) {
+	rows, err := p.Conn.QueryContext(ctx, query, args)
+
+	if err != nil {
+		logrus.Error(err)
+		return nil, err
+	}
+
+	defer func() {
+		err := rows.Close()
+		if err != nil {
+			logrus.Error(err)
+		}
+	}()
+
+	result := make([]*models.Supplier, 0)
+
+	for rows.Next() {
+		t := new(models.Supplier)
+		err = rows.Scan(&t.ID, &t.Code, &t.Name, &t.Address, &t.Status)
+
+		if err != nil {
+			logrus.Error(err)
+			return nil, err
+		}
+
+		result = append(result, t)
+	}
+
+	return result, nil
+}
+
+func (p *pgSupplierRepository) FetchAll(ctx context.Context) (res []*models.Supplier, err error) {
+	query := `SELECT id, code, name, address, status FROM suppliers`
+
+	res, err = p.fetch(ctx, query)
+
 	if err != nil {
 		return nil, err
 	}
-	defer rows.Close()
 
-	supps := make([]*models.Supplier, 0)
-	for rows.Next() {
-		sup := new(models.Supplier)
-		err := rows.Scan(&sup.ID, &sup.Code, &sup.Name, &sup.Address, &sup.Status)
-		if err != nil {
-			return nil, err
-		}
-		supps = append(supps, sup)
-	}
-
-	if err = rows.Err(); err != nil {
-		return nil, err
-	}
-
-	return supps, nil
+	return res, nil
 }
